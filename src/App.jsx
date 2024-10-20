f49089a623c1646c4e50fe89fc294bf52c46e79a
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-10-20 22:28:18 +0530

    added editor page and uuid

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Editorpage from './pages/Editorpage'
+import { Toaster } from "react-hot-toast";
 function App() {
   
   return (
     <>
+      <div>
+        <Toaster
+          position="top-right"
+        >
+        </Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+    const [roomID, setroomID] = useState("");
+    const [username, setusername] = useState("");
+
+    const createRoom= (e) => {
+        e.preventDefault();
+        const id = v4();
+        console.log(id);
+        setroomID(id);
+        toast.success("Created a new room",{
+            style:{
+                border: '2px solid #EF00DE',
+                padding: '16px',
+                color: 'white',
+                background: '#282a36'
+            },
+            iconTheme: {
+                primary: 'green',
+                secondary: 'white',
+            },
+        })
+    }
+    const joinRoom = () =>{
+        if(!roomID || !username){
+            toast.error("Room ID and Username is required",{
+                style:{
+                    border: '2px solid #EF00DE',
+                    padding: '16px',
+                    color: 'white',
+                    background: '#282a36'
+                },
+                iconTheme: {
+                    primary: 'red',
+                    secondary: 'white',
+                },
+            })
+            return;
+        }
+        navigate(`editor/${roomID}`,{
+            state: {
+                username
+            }
+        })
+
+    }
+    const enterpressed = (e) => {
+        if(e.code == "Enter"){
+            joinRoom();
+        }
+    }
     return (
         <div className="homepagewrapper">
             <div className="formwrapper">
                 <img src="codev.png" alt="Logo" className="logo"/>
                 <h4 className="heading">Enter the Room ID to start collaborating</h4>
                 <div className="inputwrapper">
-                    <input type="text" className="inputbox" placeholder="Room ID"/>
-                    <input type="text" className="inputbox" placeholder="Username"/>
-                    <button className="btn joinBtn">Join Now</button>
+                    <input 
+                    type="text" className="inputbox" 
+                    value={roomID} placeholder="Room ID" 
+                    onChange={(e) => setroomID(e.target.value)}
+                    onKeyDown={enterpressed}
+                    />
+                    <input 
+                    type="text" className="inputbox" placeholder="Username"
+                    value={username}
+                    onChange={(e) => setusername(e.target.value)}
+                    onKeyDown={enterpressed}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>Join Now</button>
 
                     <span className="newroom">Don't have a Room ID? &nbsp; 
-                        <a href="" className="createnewBtn">
+                        <a href="" onClick= {createRoom} className="createnewBtn">
                             create new room
                         </a>
                     </span>
